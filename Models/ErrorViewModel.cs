dba9cd753a7ee5e8e3ce54cc84d49ce3ebade8f2
User <User@CECILSLAPPY>
2024-06-19 09:32:04 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ForReference_Surriga.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
